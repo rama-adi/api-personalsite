7fa85cf31464faf1482e16bb5168bf091a5b550f
rama-adi <17980909+rama-adi@users.noreply.github.com>
2022-08-01 23:34:07 +0800

    wip

--- app/Http/Controllers/API/v1/PostController.php
+++ app/Http/Controllers/API/v1/PostController.php
@@ -3,13 +3,20 @@
 namespace App\Http\Controllers\API\v1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\v1\PostIndexResource;
+use App\Http\Resources\v1\PostShowResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
-        return response()->json(Post::all());
+        return PostIndexResource::collection(Post::with('user')->paginate(10));
+    }
+
+    public function show(Post $post)
+    {
+        return new PostShowResource($post);
     }
 }
